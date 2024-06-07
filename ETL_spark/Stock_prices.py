dd9e7a75443a2a675086d925654ccc031523fc6f
Adrian <105028082+AdrianChen0125@users.noreply.github.com>
2024-06-07 13:13:23 +0100

    Add files via upload

---
+++ ETL_spark/Stock_prices.py
@@ -0,0 +1,62 @@
+# Databricks notebook source
+from pyspark.sql.types import *
+from pyspark.sql import functions as f
+import requests
+import pandas as pd 
+
+# COMMAND ----------
+
+# get data from api 
+def get_stock_data(symbol,apikey,data_size):
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={0}&apikey={1}&outputsize={2}'.format(symbol,apikey,data_size)
+    r = requests.get(url)
+    data = r.json()
+    return data
+# read data into pandas df and change schema and add symbol
+def transform_stock_data(data):
+    df = (
+        pd.DataFrame(data['Time Series (Daily)'])
+        .transpose()
+        .reset_index()
+        .rename(columns={"index":'date',"1. open":'open',"2. high":"high","3. low":"low","4. close":"close","5. volume":"volume"})
+    )
+    # add symbol
+    df['symbol']=data['Meta Data']['2. Symbol']
+    return df
+
+# join data frame and load into csv
+def concate (dataframe_list):
+    united_df = pd.concat(dataframe_list)
+    return united_df
+
+# turn pandas df into spark datafrme and change scheme
+def read_into_sp(dataframe):
+    result = (
+    spark.createDataFrame(dataframe)
+    .withColumn("date", f.to_date(f.col('date'), "yyyy-MM-dd"))
+    .withColumn("open",f.col('open').cast(FloatType()))
+    .withColumn("high",f.col('high').cast(FloatType()))
+    .withColumn("low",f.col('low').cast(FloatType()))
+    .withColumn("close",f.col('close').cast(FloatType()))
+    .withColumn("volume",f.col('volume').cast(FloatType()))
+    )
+    return result
+
+
+# COMMAND ----------
+
+if __name__ == "__main__":
+    # adls_path = "adl://<your-storage-account>.azuredatalakegen2.net/<path-to-folder>/output.parquet"
+    apikey='YGCUCM7FNF36SZJ0'
+    #data size compact = 100 data points and full data from previous 20 years
+    data_size='full'
+    # set up target ticker 
+    symbol_list=['NVDA','AMD','INTC','QCOM','GOOG','MSFT','AMZN','AAPL']
+    # get data from api
+    stock_data = [get_stock_data(symbol,apikey,data_size) for symbol in symbol_list]
+    # transfrom json into data frame
+    transformed_df_list = [transform_stock_data(data) for data in stock_data]
+    final_df = concate_df (transformed_df_list)
+    final_df_sp = read_into_sp(final_df)
+    print('load data into filepath')
+    # final_df_sp.write.parquet(adls_path)
