94fb10bc1749b894b608e53ecff4915d9fa86402
Adrian <105028082+AdrianChen0125@users.noreply.github.com>
2024-06-21 17:13:34 +0100

    Add files via upload

---
+++ create_key.ps1
@@ -0,0 +1,5 @@
+az login
+az keyvault secret set --vault-name synapse-key123 --name "alphavantage-key" --value "YGCUCM7FNF36SZJ0"
+az keyvault secret set --vault-name synapse-key123 --name "fmp-key" --value "Zw1r6iuegK2CX3beasj6nxAkrllOCkoy"
+az keyvault secret set --vault-name synapse-key123 --name "reddit-key" --value "cKPHnxi_E9gr7HM5DKdO-XU7nFdO2w"
+az keyvault secret set --vault-name synapse-key123 --name "youtube-key" --value "AIzaSyBX_jEiRD49Pp-7r3nm1YOz3p-S0cUnPRs"
